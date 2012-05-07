2bbcc3ba7c959a60ba037e171c017bc8a57ff165
Dan Harden <daniel.harden@gmail.com>
2012-05-07 14:48:53 -0400

    Widgetizing (I hate that word) media bar

--- sidebar-media.php
+++ sidebar-media.php
@@ -1,9 +1,13 @@
 		<aside id="media" class="span2">
-          <img src="<?php echo THEME_IMAGES; ?>/collage.jpg" alt="Photo Gallery" />
-          <h3>Check out our photo gallery!</h3>
-          <p>See photos of our robots, events, and competitions.</p>
-          <img src="<?php echo THEME_IMAGES; ?>/Feed_512x512.png" alt="RSS Feed" width="48" height="48" />
-          <img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="RSS Feed" width="48" height="48" />
-          <img src="<?php echo THEME_IMAGES; ?>/Twitter_512x512.png" alt="RSS Feed" width="48" height="48" />
-          <img src="<?php echo THEME_IMAGES; ?>/Youtube_512x512.png" alt="RSS Feed" width="48" height="48" />
+	      <img src="<?php echo THEME_IMAGES; ?>/collage.jpg" alt="Photo Gallery" />
+	      <h3>Check out our photo gallery!</h3>
+	      <p>See photos of our robots, events, and competitions.</p>
+	      <img src="<?php echo THEME_IMAGES; ?>/Feed_512x512.png" alt="RSS Feed" width="48" height="48" />
+	      <img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="RSS Feed" width="48" height="48" />
+	      <img src="<?php echo THEME_IMAGES; ?>/Twitter_512x512.png" alt="RSS Feed" width="48" height="48" />
+	      <img src="<?php echo THEME_IMAGES; ?>/Youtube_512x512.png" alt="RSS Feed" width="48" height="48" />
+
+      		<?php if ( function_exists ( dynamic_sidebar("media") ) ) : dynamic_sidebar ("media");
+	      		endif;
+			?>
         </aside>
