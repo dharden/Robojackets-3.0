ab39e0ec3f5f0e8ad6fec07ae7e32b654027480d
Dan Harden <daniel.harden@gmail.com>
2012-05-07 22:55:06 -0400

    Setting Featured Image size to full

--- carousel.php
+++ carousel.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php $count = 1; ?>
   <div class="item <?php if ($count == 1) { echo 'active'; }l ?>">
-    <?php if ( has_post_thumbnail() ) { the_post_thumbnail( array(406, 908) ); } ?>
+    <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full' ); } ?>
     <div class="carousel-caption">
       <h4><?php the_title(); ?></h4>
       <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
