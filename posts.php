1118621fc7c1ae2792912dc462db96386876b2bd
dharden <redmonkey@gmail.com>
2012-05-08 01:29:41 -0300

    Fixing loop? Who can say!

--- posts.php
+++ posts.php
@@ -1,4 +1,4 @@
-<?php query_posts('cat='); ?>
+<?php query_posts(querystring); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?> class="post">
 	    <header class="row">
