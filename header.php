4bdca2cd30fa7adbe7c237dcb54e417acb3f81cc
dharden <redmonkey@gmail.com>
2012-05-14 11:16:41 -0300

    Fixing html title to display blog title

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?php wp_title( ' - ', true, 'right' ); ?></title>
+    <title><?php wp_title( ' - ', true, 'right' ); bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
