bb86bf081d7cd815124ef731c7f9378af9d01a09
Dan Harden <daniel.harden@gmail.com>
2012-05-13 23:50:03 -0400

    Added blurb to carousel

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form id="searchform" class="navbar-search pull-right" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+                  <input type="text" class="search-query" placeholder="Search" value="" name="s" id="s">
+                  <button type="submit" class="btn-mini"><i class="icon-search"></i></button>
+                </form>
