49d8aa279ffa0a16e86e1c3470b543c1a10732a0
Dan Harden <daniel.harden@gmail.com>
2012-05-13 23:22:00 -0400

    Added search functionality

--- index.php
+++ index.php
@@ -50,8 +50,9 @@
                     </ul>
                   </li>
                 </ul> -->
-                <form class="navbar-search pull-right">
-                  <input type="text" class="search-query" placeholder="Search">
+                <form id="searchform" class="navbar-search pull-right" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+                  <input type="text" class="search-query" placeholder="Search" value="" name="s" id="s">
+                  <button type="submit" class="btn-mini"><i class="icon-search"></i></button>
                 </form>
               </div>
             </div>
