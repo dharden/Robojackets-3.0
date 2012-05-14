983ab519c52f212caa53ef9c39edf6ea39fd8c6a
Dan Harden <daniel.harden@gmail.com>
2012-05-13 23:49:56 -0400

    Added blurb to carousel

--- carousel.php
+++ carousel.php
@@ -7,10 +7,10 @@
 		<?php if ( $carousel_query-> have_posts()) : while ($carousel_query-> have_posts()) : $carousel_query-> the_post(); ?>
 		  <div class="item <?php if ($count == 1) { echo 'active'; }l ?>">
 		    <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full' ); } ?>
-		    <div class="carousel-caption">
+    		<div class="carousel-caption">
 		      <h4><?php the_title(); ?></h4>
-		      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-		    </div>
+		      <p><?php $key="blurb"; echo get_post_meta($post->ID, $key, true); ?> <a href="<?php the_permalink(); ?>">Read more...</a></p>
+	    	</div>
 		  </div>
 		<?php $count++ ; ?>
 		<?php endwhile; ?>
--- index.php
+++ index.php
@@ -50,10 +50,7 @@
                     </ul>
                   </li>
                 </ul> -->
-                <form id="searchform" class="navbar-search pull-right" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-                  <input type="text" class="search-query" placeholder="Search" value="" name="s" id="s">
-                  <button type="submit" class="btn-mini"><i class="icon-search"></i></button>
-                </form>
+                <?php get_search_form( $echo ); ?>
               </div>
             </div>
           </div>
