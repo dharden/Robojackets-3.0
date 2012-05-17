6402f52cad61d62d1d45474e119ab20a785d7abf
Dan Harden <daniel.harden@gmail.com>
2012-05-17 16:15:17 -0400

    Added template for pages. Right now just a modified index.

---
+++ page.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+        <nav id="sitenav" class="navbar navbar-fixed-top">
+          <div class="navbar-inner">
+            <div class="container">
+              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <div class="nav-collapse">
+                <?php wp_nav_menu( array( 'theme_location' => 'navigation', 'container' => false ) ); ?>
+
+                <?php get_search_form( $echo ); ?>
+              </div>
+            </div>
+          </div>
+        </nav> <!-- /sitenav -->
+
+      </header> <!-- /siteheader -->
+      <!-- Carousel for featured news items -->
+
+        <?php if ( is_front_page() ) { include("carousel.php"); } ?>
+
+      <!-- Main content (posts, info, etc) -->
+      <section id="content" class="row">
+
+      <!-- Info about joining the club -->
+      <?php get_sidebar(clubinfo); ?>
+
+        <!-- News items -->
+        <section id="posts" class="span7">
+
+        <?php include("pages.php"); ?>
+
+        </section> <!-- /posts -->
+
+        <!-- Media items (photo gallery, social media, etc.) -->
+        <?php get_sidebar(media); ?>
+        
+      </section> <!-- /content -->
+
+      <hr>
+<?php get_footer(); ?>
---
+++ pages.php
@@ -0,0 +1,19 @@
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<article id="post-<?php the_ID(); ?>" class="post">
+	    <header class="row">
+	      <div class="span1 icon" style="margin: 0 -20px 0 20px;">
+	        <?php getCategoryImage() ; ?>
+	      </div>
+	      <div class="span6">
+	        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+	        <p><?php edit_post_link('edit', '', ''); ?></p>
+	      </div>
+	    </header>
+	    <?php the_content(); ?>
+	    <?php wp_link_pages('before=<div id="page-links">&after=</div>'); ?>
+	    <p><?php the_category(', '); ?></p>
+	</article>
+	<hr />
+<?php endwhile; ?>
+<?php endif; ?>
+<?php posts_nav_link('after=<hr />'); ?>
