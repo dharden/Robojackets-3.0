f3adf65a5bd5002fe1f928da5727d6124e52d4de
Dan Harden <daniel.harden@gmail.com>
2012-05-17 12:52:51 -0400

    Added link to about/join pages in sidebar

--- sidebar-clubinfo.php
+++ sidebar-clubinfo.php
@@ -1,7 +1,7 @@
         <section id="clubinfo" class="span3 sidebar">
 
-         	<img src="<?php echo THEME_IMAGES; ?>/who.png" alt="who?" width="220" />
-        	<img src="<?php echo THEME_IMAGES; ?>/how.png" alt="how?" width="202" />
+         	<a href="<?php echo esc_url( get_permalink( get_page_by_title( 'About' ) ) ); ?>"><img src="<?php echo THEME_IMAGES; ?>/who.png" alt="who?" width="220" /></a>
+        	<a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Join Us' ) ) ); ?>"><img src="<?php echo THEME_IMAGES; ?>/how.png" alt="how?" width="202" /></a>
 
       		<?php if ( function_exists ( dynamic_sidebar("clubinfo") ) ) : ?>
 			<?php dynamic_sidebar ("clubinfo"); ?>
