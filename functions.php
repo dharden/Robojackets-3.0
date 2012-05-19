726ebfe9b7141c5e68052f9c348783b55245dc30
Dan Harden <daniel.harden@gmail.com>
2012-05-19 15:53:22 -0400

    Completely removed jQuery navbar customization-- now written purely as custom nav walkers in php

--- functions.php
+++ functions.php
@@ -80,25 +80,71 @@ if ( function_exists( 'add_image_size' ) ) {
 
 class Bootstrapifier extends Walker_Nav_Menu {
 
+	function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ){
+	    if ( isset($children_elements[$element->ID]) ) {
+	    	//$args[$element->ID]->link_before = "hello";
+	    	$GLOBALS['after'] = 'rofl';
+	    	$GLOBALS['dd_children'] = 1;
+	    }
+	    else {
+	    	$GLOBALS['dd_children'] = 0;
+	    };
+	    //var_dump($args);
+        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    }
+
 	function start_lvl(&$output, $depth) {
 		$indent = str_repeat("\t", $depth);
 		$output .= "\n$indent<ul class=\"sub-menu dropdown-menu\">\n";
 	}
+
+	function start_el(&$output, $item, $depth, $args) {
+		global $wp_query;
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$class_names = $value = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+
+		global  $dd_children;
+     	if ($dd_children) {
+     		$item_output .= ' <b class="caret"></b>';
+     	}
+
+		$item_output .= '</a>';
+
+
+ 		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
 }
 
-/* find some way to check if item is parent
+add_filter( 'nav_menu_css_class', 'add_parent_css', 10, 2 );
 
-add_filter('nav_menu_css_class', 'auto_custom_type_class', 10, 2 );
-function auto_custom_type_class($classes, $item) {
-
-
-    if ($item-> menu_item_parent == "0"){
-        $classes[] = "dropdown";
-    }
-
+function  add_parent_css($classes, $item){
+     global  $dd_children;
+     if($dd_children)
+         $classes[] = 'dropdown';
     return $classes;
 }
-
-*/
 
 ?>
--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 $('document').ready(function() {
-	
+
 	$('#carousel').carousel({
   	interval: 5000
 	});
-
-	$("#menu-nav li").has("ul").addClass("dropdown");
-	$('#menu-nav li.dropdown > a').append(' <b class="caret"></b>');
-
-	}
+	
 )
