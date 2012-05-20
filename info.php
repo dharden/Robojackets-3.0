ed59bce6b8784a5edff1e6b9e4825d7345b58665
Dan Harden <daniel.harden@gmail.com>
2012-05-20 15:43:07 -0400

    Hiding info if not front page

--- info.php
+++ info.php
@@ -1,1 +1,17 @@
-<? phpinfo(); ?>
+      <section id="info" class="row">
+        <div class="span4">
+          <h2>Who are we?</h2>
+          <p>The RoboJackets is a group of Georgia Tech students, faculty, and alumni that aims to enhance the understanding of the field of robotics and its applications in depth of knowledge as well as to increase of the number of students exposed to it.</p>
+          <a class="btn" href="#">Find out more »</a>
+        </div>
+        <div class="span4">
+          <h2>What do we do?</h2>
+          <p>We fuck robots and chew gum... and we're all out of gum.</p>
+          <a class="btn" href="#">View details »</a>
+        </div>
+        <div class="span4">
+          <h2>How do you get involved?</h2>
+          <p>It's easy-- no experience required. We'll teach you everything you need to know. What are you waiting for? Join us.</p>
+          <a class="btn" href="#">Find out more »</a>
+        </div>
+      </section>
