de1022be5c652a3434c33849f307d4ec7fdcbbe0
Dan Harden <daniel.harden@gmail.com>
2012-05-20 15:06:24 -0400

    Moving stuff around

--- sidebar-clubinfo.php
+++ sidebar-clubinfo.php
@@ -1,12 +1,13 @@
         <section id="clubinfo" class="span3 sidebar">
 
-    	  <a href="<?php echo HOME_URI; ?>/gallery"><img src="<?php echo THEME_IMAGES; ?>/collage.jpg" alt="Photo Gallery" /></a>
-	      <p><strong>Check out our photo gallery!</strong></p>
-	      <p>See photos of our robots, events, and competitions.</p>
+          <h3>Follow Us</h3>
 	      <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo THEME_IMAGES; ?>/Feed_512x512.png" alt="RSS Feed" width="48" height="48" /></a>
 	      <a href="https://www.facebook.com/groups/27431371825/"><img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="Facebook" width="48" height="48" /></a>
 	      <a href="http://twitter.com/#!/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/Twitter_512x512.png" alt="Twitter" width="48" height="48" /></a>
 	      <a href="http://www.youtube.com/user/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/Youtube_512x512.png" alt="YouTube" width="48" height="48" /></a>
+
+	      <h3>Photos</h3>
+    	  <a href="<?php echo HOME_URI; ?>/gallery"><img src="<?php echo THEME_IMAGES; ?>/collage.jpg" alt="Photo Gallery" width="150" /></a>
 
       		<?php if ( function_exists ( dynamic_sidebar("clubinfo") ) ) : ?>
 			<?php dynamic_sidebar ("clubinfo"); ?>
