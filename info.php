3b150f43e87b67cab864a74be434f7e28ca6821a
Dan Harden <daniel.harden@gmail.com>
2012-05-20 22:46:38 -0400

    Added links to info pages

--- info.php
+++ info.php
@@ -2,16 +2,16 @@
         <div class="span4">
           <h2>Who are we?</h2>
           <p>The RoboJackets is a group of Georgia Tech students, faculty, and alumni that aims to enhance the understanding of the field of robotics and its applications in depth of knowledge as well as to increase of the number of students exposed to it.</p>
-          <a class="btn" href="#">Find out more »</a>
+          <a class="btn" href="<?php echo esc_url( get_permalink( get_page_by_title( 'About' ) ) ); ?>">Find out more »</a>
         </div>
         <div class="span4">
           <h2>What do we do?</h2>
           <p>We fuck robots and chew gum... and we're all out of gum.</p>
-          <a class="btn" href="#">View details »</a>
+          <a class="btn" href="<?php echo esc_url( get_permalink( get_page_by_title( 'Teams' ) ) ); ?>">See our teams »</a>
         </div>
         <div class="span4">
           <h2>How do you join?</h2>
           <p>It's easy-- no experience required. We'll teach you everything you need to know. What are you waiting for? Join us.</p>
-          <a class="btn" href="#">Find out more »</a>
+          <a class="btn" href="<?php echo esc_url( get_permalink( get_page_by_title( 'Join Us' ) ) ); ?>">Join us! »</a>
         </div>
       </section>
