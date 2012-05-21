c693d4b64230449ea724e7036f9ed559057eaebe
dharden <redmonkey@gmail.com>
2012-05-21 17:11:33 -0300

    Changed flavor text.

--- info.php
+++ info.php
@@ -1,12 +1,12 @@
       <section id="info" class="row">
         <div class="span4">
           <h2>Who are we?</h2>
-          <p>The RoboJackets is a group of Georgia Tech students, faculty, and alumni that aims to enhance the understanding of the field of robotics and its applications in depth of knowledge as well as to increase of the number of students exposed to it.</p>
+          <p>The RoboJackets is a group of Georgia Tech students, faculty, and alumni. We want to enhance the understanding of the field of robotics and to increase of the number of students exposed to it.</p>
           <a class="btn" href="<?php echo esc_url( get_permalink( get_page_by_title( 'About' ) ) ); ?>">Find out more »</a>
         </div>
         <div class="span4">
           <h2>What do we do?</h2>
-          <p>We fuck robots and chew gum... and we're all out of gum.</p>
+          <p>We compete in several robotics competitions, with robots ranging from small, autonomous soccer players to large, human operated machines.</p>
           <a class="btn" href="<?php echo esc_url( get_permalink( get_page_by_title( 'Teams' ) ) ); ?>">See our teams »</a>
         </div>
         <div class="span4">
