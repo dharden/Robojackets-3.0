09922a82cd6f6c0239bed78a01556079f5337692
dharden <redmonkey@gmail.com>
2012-05-21 17:12:32 -0300

    Update info.php

--- info.php
+++ info.php
@@ -1,7 +1,7 @@
       <section id="info" class="row">
         <div class="span4">
           <h2>Who are we?</h2>
-          <p>The RoboJackets is a group of Georgia Tech students, faculty, and alumni. We want to enhance the understanding of the field of robotics and to increase of the number of students exposed to it.</p>
+          <p>The RoboJackets is a group of Georgia Tech students, faculty, and alumni. We want to enhance the understanding of robotics and to increase of the number of students exposed to it.</p>
           <a class="btn" href="<?php echo esc_url( get_permalink( get_page_by_title( 'About' ) ) ); ?>">Find out more »</a>
         </div>
         <div class="span4">
