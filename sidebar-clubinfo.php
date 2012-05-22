ab3e809020f22960fa21fa3f76c3ee9fa360c79c
dharden <redmonkey@gmail.com>
2012-05-21 22:46:42 -0300

    Update sidebar-clubinfo.php

--- sidebar-clubinfo.php
+++ sidebar-clubinfo.php
@@ -1,8 +1,9 @@
         <section id="clubinfo" class="span3 sidebar">
 
 	  	  <aside ID="photos" class="widget">
-		      <h3>Photos</h3>
+		      <h3>Photo Gallery</h3>
 	    	  <a href="<?php echo HOME_URI; ?>/gallery"><img src="<?php echo THEME_IMAGES; ?>/collage.jpg" alt="Photo Gallery" width="150" /></a>
+	    	  <p>Check out photos of our robots, events, and more.</p>
     	  </aside>
       		<?php if ( function_exists ( dynamic_sidebar("clubinfo") ) ) : ?>
 			<?php dynamic_sidebar ("clubinfo"); ?>
