1b00192aa11261abebb5cbad888b595ef33bb679
Hunter Scott <hunter.scott@gatech.edu>
2013-08-06 10:13:20 -0400

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Robojackets-3.0
+ */
+
+get_header();
+?>
+
+	<div id="content" class="narrowcolumn">
+
+		<h1 style="text-align:center">That page doesn't exist. Try heading back to the <a href="http://robojackets.org">home page</a>.</h1>
+<h2 style="text-align:center" class="center">Error 404 - Not Found</h2>
+
+
+	</div>
